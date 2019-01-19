4691e8799810c0dcb1110305b8ddaeb7ad91ed85
Nsonsa <elombekisala@gmail.com>
2019-01-19 03:59:43 -0500

    Created a student dictionary to hold their names and grades. Then diplayed the list of students to user when they chose the option in the menu.

--- NsonsaKisala_Exercise3/Program.cs
+++ NsonsaKisala_Exercise3/Program.cs
@@ -13,9 +13,16 @@ namespace NsonsaKisala_Exercise3
             // Welcome statement
             Console.Clear();
             Console.WriteLine("Welcome to Coffee High \nThe Place to learn anything and everything about coffee! \n");
-            
 
-           
+            // Student Dictionary (names, grades)
+            Dictionary<string, double> studentDict = new Dictionary<string, double>();
+
+            studentDict.Add("Michael Scott", 89);
+            studentDict.Add("Dwight Schrute", 99.99);
+            studentDict.Add("Jim Halpert", 100);
+            studentDict.Add("Andy Bernard", 74);
+            studentDict.Add("Toby Flenderson", 40);
+
             bool isValid = true;
             // Switch Statement for userSelect
             while (isValid)
@@ -26,15 +33,22 @@ namespace NsonsaKisala_Exercise3
                 {
                     case "1":
                     case "see list of students":
-                        Console.WriteLine("1 works");
+                        Console.WriteLine("Students: \n");
+
+                        // Display each student(key) in studentDict to the console
+                        foreach( KeyValuePair< string, double> kvp in studentDict)
+                        {
+                            Console.WriteLine(kvp.Key);
+                        }
+
                         break;
                     case "2":
                     case "see student gpas":
-                        Console.WriteLine("2 works");
+                        Console.WriteLine("GPA: \n");
                         break;
                     case "3":
                     case "edit student grades":
-                        Console.WriteLine("3 works");
+                        Console.WriteLine("Edit: \n");
                         break;
                     case "4":
                     case "exit":
